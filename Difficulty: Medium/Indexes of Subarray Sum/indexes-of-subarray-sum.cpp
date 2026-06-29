4f1cba74cb426f0c2b4993ab28bae944c207debb
Dilnawaz Ansari <dilnawazansari401@gmail.com>
2026-06-29 22:12:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> subarraySum(vector<int> &arr, int target) {
+        int n = arr.size();
+        int start = 0;
+        long long sum = 0;
+
+        for (int end = 0; end < n; end++) {
+            sum += arr[end];
+
+            while (sum > target && start <= end) {
+                sum -= arr[start];
+                start++;
+            }
+
+            if (sum == target) {
+                return {start + 1, end + 1}; // 1-based indexing
+            }
+        }
+
+        return {-1};
+    }
+};
